4b64a94d6ad776e61659a83b2a737bdc32fa2b1c
Tom van Tilburg <tom.van.tilburg@gmail.com>
2016-01-20 16:20:57 +0100

    Added cow, not working yet

--- sensorpeer.js
+++ sensorpeer.js
@@ -1,10 +1,12 @@
-configfile = process.argv[2];
+configfile = './config.json';
 //Enable full path
 config = require(configfile);
 
-_ = require('./node_modules/underscore/underscore.js')._
-Promise = require('./node_modules/es6-promise').Promise;
+_ = require('underscore/underscore.js')._
+Promise = require('es6-promise').Promise;
 Events = require('./events.js');
+http = require('http'),
+Cam = require('onvif').Cam;
 
 WebSocket = require('websocket').client;
 pg = require('pg').native;
@@ -12,8 +14,8 @@ pg = require('pg').native;
 GLOBAL.dbUrl = config.dbUrl;
 //Set env var to accept all certificates
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-Cow = require('./bower_components/cow/dist/cow.node.js');
-
+//Cow = require('./bower_components/cow/dist/cow.node.js');
+Cow = require('/var/data/sites/cow/dist/cow.nodb.js');
 
 core = new Cow.core({
     herdname: config.herdname,
@@ -39,4 +41,41 @@ core.projectStore().loaded.then(function(){
 	console.log(core.projects().length, ' projects loaded');
 	core.peer().data('superpeer', true).sync();
 	console.log('My peerid: ', core.peerid());
+	
+	core.projects().forEach(function(p){
+		p.itemStore().on('datachange',function(){
+			var cambait = p.items().filter(function(item){
+				return item.data('type') == 'cambait';
+			});
+			if (cambait.lenght > 0){
+				console.log('start following ',cambait[0].id());
+			}
+		});
+	});
+	
+});
+
+
+
+var entrancecam = new Cam({
+  hostname: '192.168.26.104',
+  username: 'onvifuser',
+  password: 'onvifuser',
+}, function(err) {
+  this.absoluteMove({x: 1, y: 1, zoom: 1});
+  this.getStatus(function(req,res){
+  	console.log(req,res);
+  });
+  
+  
+  
+  /*
+  this.getStreamUri({protocol:'RTSP'}, function(err, stream) {
+    http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end('<html><body>' +
+        '<embed type="application/x-vlc-plugin" target="' + stream.uri + '"></embed>' +
+        '</body></html>');
+    }).listen(3389);
+  });*/
 });
